a4284c9010cadbac3fdbdda47539e88d194ebd29
Deanna Polintan <deanna@Deannas-MacBook-Pro.local>
2022-09-24 03:31:49 -0400

    First commit

---
+++ src/components/Table.jsx
@@ -0,0 +1,12 @@
+import TableHead from "./TableHead";
+import TableBody from "./TableBody";
+const Table = () => {
+  return (
+    <table>
+      <TableHead />
+      <TableBody />
+    </table>
+  );
+};
+
+export default Table;
